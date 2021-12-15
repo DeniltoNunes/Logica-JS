3feeafb3e0a40567704cca1c5ac5fdd89cca1503
Denilton Nunes <denilton.nunesm@gmail.com>
2021-12-15 12:03:01 -0300

    Separação JavaScript.
    Separação do JavaScript e HTML;
    Exercícios 1 e 2.

---
+++ exercicios/AvaliacaoLogicaDeProgramação/JavaScript/exercicio2.js
@@ -0,0 +1,8 @@
+//2. Faça um programa mostrando a tabuada do 5 com os números de 1 a 10.
+
+alert('Eu vou te falar a tabuada 😃')
+let contador = 1;
+while (contador <= 10) {
+    alert('5 X ' + contador + ' = ' + (contador * 5));
+    contador += 1;
+}
