ffb74259c781e28e7792c8ea0c075f0edfd1ed6c
Denilton Nunes <denilton.nunesm@gmail.com>
2021-12-15 14:12:23 -0300

    Separação JavaScript.
    Separação do JavaScript e HTML;
    Exercício 19.

---
+++ exercicios/AvaliacaoLogicaDeProgramação/JavaScript/exercicio19.js
@@ -0,0 +1,6 @@
+let a = prompt('Digite o valor (númerico) de A: ');
+let b = prompt('Digite o valor (númerico) de B: ');
+let troca = 0;
+
+alert('O novo valor de A é: ' + b);
+alert('O novo valor de B é: ' + a);
