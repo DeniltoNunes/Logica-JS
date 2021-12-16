51b189cf32004e2ca6a0e229ceddac11811c6984
Denilton Nunes <denilton.nunesm@gmail.com>
2021-12-16 10:48:14 -0300

    Atualização JavaScript.
    Atualização JavaScript;
    Exercícios 9 e 10.

--- exercicios/Condicional/JavaScript/exercicio10.js
+++ exercicios/Condicional/JavaScript/exercicio10.js
@@ -1,9 +1,11 @@
 let contador = 1;
 let total = 0
 let ins = 0
+
 while (contador <= 4) {
     let numero = Number(prompt("Digite o " + contador + "º valor"));
     contador += 1;
+
     if (numero >= 0 && numero <= 10) {
         total += numero;
         ins += 1;
@@ -12,8 +14,11 @@ while (contador <= 4) {
 
 let med = (total / ins)
 
-if (med >= 5) {
-    document.write("Você passou no teste")
-} else {
-    document.write("Tente novamente")
+switch (true) {
+    case (med >= 5):
+        alert("Você passou no teste");
+        break;
+    default:
+        alert("Tente novamente");
+        break;
 }
--- exercicios/Condicional/JavaScript/exercicio9.js
+++ exercicios/Condicional/JavaScript/exercicio9.js
@@ -1,13 +1,15 @@
 let contador = 1;
 let valoresInseridos = ""
 let total = 0
+
 while (contador <= 6) {
-    let numero = Number(prompt("Digite o " + contador + "º valor \nVALORES MAIORES QUE 72 NÃO SERÃO SOMADOS"));
+    let numero = Number(prompt("Digite o " + contador + "º valor \nObs: Valores maiores que 72 não serão somados."));
     valoresInseridos = valoresInseridos + (`  ${numero}`);
     contador += 1;
+
     if (numero < 72) {
         total += numero;
     }
 }
 
-document.write("A soma dos valores inseridos \u00e9: " + total + "<br>Os valores inseridos foram:" + valoresInseridos);
+alert("A soma dos valores inseridos é: " + total + "\nOs valores inseridos foram:" + valoresInseridos);
